189cee4a363b4411e6e7a737617bcbe340ea9edf
Qbit <billchifer@bk.ru>
2025-10-01 11:37:01 +0700

    Добавляем конструкторы для класса ParserEngine. Объект класса LexerEngineAdvance и raw-токены

--- Parser/ParserEngine.hpp
+++ Parser/ParserEngine.hpp
@@ -3,19 +3,24 @@
 #define PARSER_ENGINE_HPP
 #pragma once
 
+#include "LexerEngineAdvance.hpp"
+
 class ParserEngine
 {
 public:
-	ParserEngine();
+	std::vector<LexToken> ParserEngineBuffer;
+
+	ParserEngine(const LexerEngineAdvance& advance) : 
+		ParserEngineBuffer(advance.GetBufferLexerAdvanceToken()) {};
+
+	ParserEngine(const std::vector<LexToken>& Buffer) : 
+		ParserEngineBuffer(Buffer) {};
+
 	~ParserEngine();
 
 private:
 
 };
-
-ParserEngine::ParserEngine()
-{
-}
 
 ParserEngine::~ParserEngine()
 {
