3c9580098a6e0724c0f9f07dd64e35f4bffe3158
Qbit <billchifer@bk.ru>
2025-12-19 18:19:45 +0700

    Добавляем ограниченную поддержку создание AST var-выражений без полной поддержки обработок ошибок

--- Node/Node.hpp
+++ Node/Node.hpp
@@ -15,4 +15,94 @@ public:
     virtual ~Node() = default;
 };
 
+class NodeTypeQualifier : public Node
+{
+public:
+    std::string Type;
+
+    struct Qualifers
+    {
+        bool IsConst = false;
+        bool IsRef = false;
+    } Qualifer;
+
+    std::string print() override {
+        std::string fprint = "";
+        if (Type.empty())
+            return "Type.empty()";
+        fprint = 
+            "var[" + 
+            (Qualifer.IsConst ? std::string("const ") : std::string("")) +
+            Type + 
+            (Qualifer.IsRef ? std::string("*") : std::string(""))
+            + "]";
+        return fprint;
+    };
+
+    NodeTypeQualifier(const std::string type, const Qualifers& qualifer) :
+        Type(type), Qualifer(qualifer) {};
+
+    ~NodeTypeQualifier() override {};
+};
+
+class NodeIdentifier : public Node
+{
+    std::string Name = "";
+public:
+    NodeIdentifier(const std::string& name) :
+        Name(name) {
+    };
+    std::string print() override { return Name; };
+    ~NodeIdentifier() override {
+    };
+};
+class NodeDeclaration : public Node
+{
+    NodeIdentifier* Identifier = nullptr;
+    Node* Initializer = nullptr;
+public:
+    std::string print() override {
+        std::string fprint = Identifier->print();
+        if (Initializer)
+            fprint += " = " + Initializer->print();
+        return fprint;
+    };
+    NodeDeclaration(NodeIdentifier* identifier, Node* initializer) :
+        Identifier(identifier), Initializer(initializer) {
+    };
+
+    ~NodeDeclaration() override {
+        delete Identifier; Identifier = nullptr;
+        delete Initializer; Initializer = nullptr;
+    };
+};
+
+class NodeDeclarationList : public Node
+{
+    NodeTypeQualifier* TypeQualifier = nullptr;
+    std::vector<NodeDeclaration*> DeclarationList;
+public:
+    std::string print() override {
+        std::string fprint = "";
+        if (!TypeQualifier)
+            return "!TypeQualifier";
+        fprint += TypeQualifier->print() + " ";
+        for (auto& Decl : DeclarationList)
+            if (Decl)
+                fprint += Decl->print() + ", ";
+        fprint.pop_back();
+        fprint.pop_back();
+        return fprint;
+    };
+    NodeDeclarationList(NodeTypeQualifier*& typeQualifier, const std::vector<NodeDeclaration*>& declarationList) :
+        TypeQualifier(typeQualifier), DeclarationList(declarationList) { };
+
+    ~NodeDeclarationList() override {
+        delete TypeQualifier;
+        for (auto& decl : DeclarationList) {
+            delete decl;
+        }
+    };
+};
+
 #endif // NODE_HPP
--- Parser/Parser.hpp
+++ Parser/Parser.hpp
@@ -8,6 +8,12 @@
 
 class Parser
 {
+	struct VariableDeclaration
+	{
+		std::string Name;
+		std::string Initializer;
+	};
+
 	using ParserEnginePtr = Node * (Parser::*)();
 private:
 	int PosBuffer = 0;
@@ -23,7 +29,7 @@ private:
 	{TTokenID::Var, &Parser::Var},
 	}};
 
-	Node* Var() { return nullptr; };
+	Node* Var();
 
 public:
 	std::vector<LexToken> ParserEngineBuffer;
@@ -49,13 +55,110 @@ void Parser::Init() {
 	
 	while (neof()) {
 		if (auto it = map.find(ParserEngineBuffer[PosBuffer].type); 
-		it != map.end()) (this->*it->second)();
+		it != map.end()) ast.push_back((this->*it->second)());
 		PosBuffer++;
 	}
+
+#ifdef _IOSTREAM_
+	if (true)
+		for (auto& i : ast)
+			if (i) std::cout << i->print() << "\n";
+#endif // _IOSTREAM_
+
+}
+
+Node* Parser::Var() {
+
+	auto GetToken = [&]() -> LexToken {
+		return ParserEngineBuffer[PosBuffer];
+		};
+
+	auto match = [&](TTokenID ID) -> bool {
+		return GetToken().type == ID;
+		};
+
+	auto NextToken = [&]()-> bool {
+		PosBuffer++;
+		while (match(TTokenID::LineFeed) || match(TTokenID::Space))
+			PosBuffer++;
+		return true;
+		};
+
+	
+	std::string Type = "";
+	NodeTypeQualifier::Qualifers Qualifer;
+	std::vector<VariableDeclaration> Declarations;
+
+	if (NextToken() && !match(TTokenID::LeftBracket) && NextToken())
+		return nullptr;
+	
+	while (!match(TTokenID::RightBracket) && NextToken())
+	{
+		switch (GetToken().type)
+		{
+		case TTokenID::Const: Qualifer.IsConst = true;
+			break;
+		case TTokenID::Asterisk: Qualifer.IsRef = true;
+			break;
+		case TTokenID::ScResOp: Type = "";
+			break;
+		case TTokenID::IdentifierLiteral:
+			Type = GetToken().value;
+			break;
+		}
+	}
+
+	bool IsFindEquals = false;
+
+	auto parseQualifiedName = [&]() -> std::string {
+		
+		std::string initializer = GetToken().value;
+		
+		if (!match(TTokenID::IdentifierLiteral))
+			return GetToken().value;
+
+		while (!match(TTokenID::Semicolon) && !match(TTokenID::Comma) && NextToken())
+		{
+			switch (GetToken().type)
+			{
+			case TTokenID::ScResOp: initializer = "";
+				break;
+			case TTokenID::IdentifierLiteral:
+				initializer = GetToken().value;
+				break;
+			}
+		}
+		return initializer;
+		};
+
+	while (!match(TTokenID::Semicolon) && NextToken())
+	{
+		if (!match(TTokenID::IdentifierLiteral))
+			break;
+		VariableDeclaration Decl;
+		Decl.Name = GetToken().value;
+		if (NextToken() && match(TTokenID::Equals) && NextToken())
+			Decl.Initializer = parseQualifiedName();
+		Declarations.push_back(Decl);
+	}
+
+	NodeDeclarationList* DeclarationList = nullptr;
+	NodeTypeQualifier* TypeQualifier = new NodeTypeQualifier(Type, Qualifer);
+	std::vector<NodeDeclaration*> ContainerDeclarationList;
+	for (int i = 0; i < Declarations.size(); i++)
+		ContainerDeclarationList.push_back(
+			new NodeDeclaration(new NodeIdentifier(Declarations[i].Name),
+				Declarations[i].Initializer.empty() ? nullptr :
+				new NodeIdentifier(Declarations[i].Initializer)));
+	DeclarationList = new NodeDeclarationList(TypeQualifier, ContainerDeclarationList);
+	return DeclarationList;
+
 };
 
 Parser::~Parser()
 {
+	for (auto& i : ast)
+		if (i) delete i;
 }
 
 #endif // PARSER_HPP
